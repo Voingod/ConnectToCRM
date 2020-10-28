006d1f2c986250254a3d59cbbba7fa616953e755
Богдан <voingod@outlook.com>
2020-10-28 15:11:21 +0200

    connect to crm, using application user get data from contacts, accounts, incidents and opporunities

---
+++ ConnectToCRM/Controllers/ContactsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ConnectToCRM.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ConnectToCRM.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactsController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            CrmConnection crmConnection = new CrmConnection();
+            var contacts = crmConnection.CrmRequest(
+                httpMethod: HttpMethod.Get,
+                "https://udstrialsdemo40.crm4.dynamics.com/api/data/v9.1/contacts",
+                clientId: "00aea9ee-9733-41d2-b1c7-b2d2207fb471",
+                clientSecret: "9wPio6.2O9PPE~TG5-8xA9S-usAHFA~ZL2",
+                tenantID: "29a54688-cd45-4308-9b73-435d36ddb378")
+                .Result.Content.ReadAsStringAsync();
+            // Similarly you can make POST, PATCH & DELETE requests 
+            return contacts.Result;
+        }
+    }
+}
---
+++ ConnectToCRM/Helpers/CrmConnection.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectToCRM.Helpers
+{
+    public class CrmConnection
+    {
+        private async Task<string> AccessTokenGenerator(string clientId, string clientSecret, string tenantID, string requestUri)
+        {
+            string authority = "https://login.microsoftonline.com/" + tenantID;
+            var credentials = new ClientCredential(clientId, clientSecret);
+            var authContext = new AuthenticationContext(authority);
+            var result = await authContext.AcquireTokenAsync(requestUri.Remove(requestUri.IndexOf("api")), credentials);
+            return result.AccessToken;
+        }
+
+        public async Task<HttpResponseMessage> CrmRequest(HttpMethod httpMethod, string requestUri, string clientId,
+                                                                 string clientSecret, string tenantID, string body = null)
+        {
+            // Acquiring Access Token  
+            var accessToken = await AccessTokenGenerator(clientId, clientSecret, tenantID, requestUri);
+
+            var client = new HttpClient();
+            var message = new HttpRequestMessage(httpMethod, requestUri);
+
+            // OData related headers  
+            message.Headers.Add("OData-MaxVersion", "4.0");
+            message.Headers.Add("OData-Version", "4.0");
+            message.Headers.Add("Prefer", "odata.include-annotations=\"*\"");
+
+            // Passing AccessToken in Authentication header  
+            message.Headers.Add("Authorization", $"Bearer {accessToken}");
+
+            // Adding body content in HTTP request   
+            if (body != null)
+                message.Content = new StringContent(body, Encoding.UTF8, "application/json");
+
+            return await client.SendAsync(message);
+        }
+    }
+}
